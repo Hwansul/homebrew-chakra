e54efa8fb51f8c3641144869dd5b5c24a26d4647
mindulle <mindullestudio@gmail.com>
2023-12-08 23:03:51 +0900

    Brew formula update for amulet version v0.4.17

--- amulet.rb
+++ amulet.rb
@@ -5,11 +5,11 @@
 class Amulet < Formula
   desc ""
   homepage "https://github.com/Hwansul/amulet"
-  version "0.4.16"
+  version "0.4.17"
 
   on_macos do
-    url "https://github.com/Hwansul/amulet/releases/download/v0.4.16/amulet_0.4.16_darwin_all.tar.gz"
-    sha256 "84af12b9d4dce2640597fc79442ef4e94e311798b702ce404df35753fd59ab27"
+    url "https://github.com/Hwansul/amulet/releases/download/v0.4.17/amulet_0.4.17_darwin_all.tar.gz"
+    sha256 "859b9d3c8be10f468a6afdb2bf6d8c95e9ba8bcc13f88fb3bceb3cfc96c53015"
 
     def install
       bin.install "amulet"
@@ -17,17 +17,17 @@ class Amulet < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Hwansul/amulet/releases/download/v0.4.16/amulet_0.4.16_linux_arm64.tar.gz"
-      sha256 "7498c6ac4c26613a9442ebf6a6a461b88c2aa3c3eea4cfc9f6c91e5fd83ddb1a"
+    if Hardware::CPU.intel?
+      url "https://github.com/Hwansul/amulet/releases/download/v0.4.17/amulet_0.4.17_linux_amd64.tar.gz"
+      sha256 "dbda4547eac2abf26ffc4148981917b29c3ff19678df6e968b79c1dc212cf4f5"
 
       def install
         bin.install "amulet"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/Hwansul/amulet/releases/download/v0.4.16/amulet_0.4.16_linux_amd64.tar.gz"
-      sha256 "e4841ccef7c37370b340aa1582a0a936f1ba50776ddf98b46d0c4bd3dc46018e"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/Hwansul/amulet/releases/download/v0.4.17/amulet_0.4.17_linux_arm64.tar.gz"
+      sha256 "11834e2ee48e3e12e2d10762b9bbe344ec7110c61836ffe974855e0e2b2fb657"
 
       def install
         bin.install "amulet"
