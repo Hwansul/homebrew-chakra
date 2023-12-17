49423816249291d9a83557db037d941fd654dc46
mindulle <mindullestudio@gmail.com>
2023-12-17 13:53:22 +0900

    Brew formula update for cape version v0.1.1

--- cape.rb
+++ cape.rb
@@ -5,11 +5,11 @@
 class Cape < Formula
   desc ""
   homepage "https://github.com/jipilmuk/cape"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
-    url "https://github.com/jipilmuk/cape/releases/download/v0.1.0/cape_0.1.0_darwin_all.tar.gz"
-    sha256 "bd15c835f9911929312a23cd5e6bf6ff5f5b7cb3d3eff28df8ba6afc9476dabc"
+    url "https://github.com/jipilmuk/cape/releases/download/v0.1.1/cape_0.1.1_darwin_all.tar.gz"
+    sha256 "d1d4ba36da3644fa84ef44d8a4f40c39513a0394082839af47b8edc91bfdcc72"
 
     def install
       bin.install "cape"
@@ -18,16 +18,16 @@ class Cape < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jipilmuk/cape/releases/download/v0.1.0/cape_0.1.0_linux_arm64.tar.gz"
-      sha256 "3f7f26e4ec1628fa4047c2c4c4daaa27230c1e4d0671044909989fae8a4bc46a"
+      url "https://github.com/jipilmuk/cape/releases/download/v0.1.1/cape_0.1.1_linux_arm64.tar.gz"
+      sha256 "d8ebe95fba978ffb955759ecb92a7390f944ed260a9bdc6447419d3d8394e038"
 
       def install
         bin.install "cape"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/jipilmuk/cape/releases/download/v0.1.0/cape_0.1.0_linux_amd64.tar.gz"
-      sha256 "5def53bb77ae33e88d70a70e7bc74e757ee8b20ab4913f1bc08a5788bc5bb084"
+      url "https://github.com/jipilmuk/cape/releases/download/v0.1.1/cape_0.1.1_linux_amd64.tar.gz"
+      sha256 "8dcb35a626fdb7e2c7cf7a1dc5e91ee1f614c7c5e0a18e0c8d0f64fe3a6133c2"
 
       def install
         bin.install "cape"
